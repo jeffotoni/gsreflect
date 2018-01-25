2ccf33dbedbe6ff6f94edcc6b30e286ebaab0c4c
jefferson otoni lima <jeff.otoni@gmail.com>
2018-01-24 23:15:38 -0200

    first commit

---
+++ example/main-simple.go
@@ -0,0 +1,52 @@
+/*
+* Golang reflect update struct to tag
+*
+* @package     main
+* @author      @jeffotoni
+* @size        2018
+ */
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/github.com/jeffotoni/gsrflect"
+	"reflect"
+)
+
+// const tag
+const tagName = "default"
+
+// struct config
+type Config struct {
+	Domain  string `default:"s3go.gov"`
+	Schema  string `default:"https"`
+	Port    string `default:"9002"`
+	Db      string `default:"postgresql"`
+	Cluster string `default:"10"`
+	Passwd  string `default:"x37c$%2"`
+	Login   string `default:"postgres"`
+}
+
+func main() {
+
+	c := Config{}
+
+	err := gsr.Default(&c)
+
+	if err != nil {
+
+		fmt.Println("error: ", err)
+		return
+	}
+
+	fmt.Println("Domain: ", c.Domain)
+	fmt.Println("Schema: ", c.Schema)
+	fmt.Println("Port: ", c.Port)
+	fmt.Println("Db: ", c.Db)
+	fmt.Println("Cluster: ", c.Cluster)
+	fmt.Println("Passwd: ", c.Passwd)
+	fmt.Println("Login: ", c.Login)
+
+}
---
+++ gsr-reflect.go
@@ -0,0 +1,39 @@
+/*
+* Golang reflect update struct to tag
+*
+* @package     main
+* @author      @jeffotoni
+* @size        2018
+ */
+
+package gsr
+
+func Default(s interface{}) (err error) {
+
+	st := reflect.TypeOf(s)
+
+	if st.Kind() != reflect.Ptr {
+
+		err = errors.New("Not a pointer")
+		return
+	}
+
+	refField := st.Elem()
+	if refField.Kind() != reflect.Struct {
+		err = errors.New("Not a struct")
+		return
+	}
+
+	//refValue := reflect.ValueOf(s).Elem()
+	for i := 0; i < refField.NumField(); i++ {
+
+		field := refField.Field(i)
+		// value := refValue.Field(i)
+		// kind := field.Type.Kind()
+		tagVal := field.Tag.Get(tagName)
+
+		reflect.ValueOf(s).Elem().Field(i).SetString(tagVal)
+	}
+
+	return
+}
